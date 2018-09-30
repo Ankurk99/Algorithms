84094d457ddb18b5dd732c9b2a8936565604f218
Knownymous <39901181+Ankurk99@users.noreply.github.com>
2018-10-01 00:34:12 +0530

    Updated insertionsort.py
    Fixed trailing whitespaces

--- insertion_sort/Python/insertion_sort.py
+++ insertion_sort/Python/insertion_sort.py
@@ -1,9 +1,8 @@
-def insertion_sort(arr):
-    """
+"""
     Fuction to do insertion sort.
-
     :param arr: A list of element to sort.
-    """
+"""
+def insertion_sort(arr):
     for slot in range(1, len(arr)):
         value = arr[slot]
         test_slot = slot - 1
@@ -11,15 +10,10 @@ def insertion_sort(arr):
             arr[test_slot + 1] = arr[test_slot]
             test_slot = test_slot - 1
         arr[test_slot + 1] = value
-
     return arr
-
-
 def main():
     arr = [6, 5, 4, 3, 2, 1]
     print('Sorted element using Insertion Sort: {}'.format(
               ' '.join(map(str, insertion_sort(arr)))))
-
-
 if __name__ == '__main__':
     main()
